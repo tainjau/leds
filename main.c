ce199553d38ef1dc5c99a85bb0fa7d0d22c77464
huangkunsheng <523644160@qq.com>
2018-03-07 20:54:01 +0800

    test message

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+	while(1)
+	{
+		
+		led_interval();  //第二题
+		
+		//led_blingbling(); //第三题
+		
+		//led_flowing();		//第四题    
+		
+			//把前面反斜杠去掉 就能执行了。。。
+			//如果要用第一个程序，就屏蔽掉后面两  
+			//有且只能执行一个程序
+	}
+	return 0;
+}
